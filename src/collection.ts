e21ad7d54fb0f569bb331e56b66736c3d3c81905
Niklas Ekman <nekm@netlight.com>
2026-06-24 11:50:48 +0200

    Bugfix, missing each function

--- src/collection.ts
+++ src/collection.ts
@@ -1,202 +1,215 @@
 import type { AsynkitInterface } from "./interface";
 import type {
-  Accumulator,
-  AsyncOrIterable,
-  Filter,
-  KeySelector,
-  Map,
-  Predicate,
-  PromiseOrValue,
+	Accumulator,
+	AsyncOrIterable,
+	Filter,
+	KeySelector,
+	Map,
+	Predicate,
+	PromiseOrValue,
 } from "./types";
 import {
-  asynkitAppend,
-  asynkitChunk,
-  asynkitConcat,
-  asynkitEvery,
-  asynkitFilter,
-  asynkitFirst,
-  asynkitFirstOrDefault,
-  asynkitFlatMap,
-  asynkitFlatten,
-  asynkitLimit,
-  asynkitFromArray,
-  asynkitFromIterable,
-  asynkitIsEmpty,
-  asynkitMap,
-  asynkitPrepend,
-  asynkitReduce,
-  asynkitSome,
-  asynkitToArray,
-  asynkitToObject,
+	asynkitAppend,
+	asynkitChunk,
+	asynkitConcat,
+	asynkitEvery,
+	asynkitFilter,
+	asynkitFirst,
+	asynkitFirstOrDefault,
+	asynkitFlatMap,
+	asynkitFlatten,
+	asynkitLimit,
+	asynkitFromArray,
+	asynkitFromIterable,
+	asynkitIsEmpty,
+	asynkitMap,
+	asynkitPrepend,
+	asynkitReduce,
+	asynkitSome,
+	asynkitToArray,
+	asynkitToObject,
+	asynkitEach,
 } from "./functions";
 
 /**
  * Main implementation of an async collection
  */
 export class Asynkit<TInput> implements AsynkitInterface<TInput> {
-  constructor(private readonly it: AsyncOrIterable<TInput>) {
-  }
+	constructor(private readonly it: AsyncOrIterable<TInput>) {}
 
-  /**
-   * Create a new instance and return an interface
-   * @param it
-   */
-  static create<TInput>(it: AsyncOrIterable<TInput>): AsynkitInterface<TInput> {
-    if (it instanceof Asynkit) {
-      return it;
-    }
+	/**
+	 * Create a new instance and return an interface
+	 * @param it
+	 */
+	static create<TInput>(it: AsyncOrIterable<TInput>): AsynkitInterface<TInput> {
+		if (it instanceof Asynkit) {
+			return it;
+		}
 
-    return new Asynkit<TInput>(it);
-  }
+		return new Asynkit<TInput>(it);
+	}
 
-  /**
-   * Creates a new instance from an array
-   * @param array
-   * @alias fromIterable
-   */
-  static fromArray<TInput>(
-    array: ReadonlyArray<TInput>,
-  ): AsynkitInterface<TInput> {
-    return new Asynkit<TInput>(asynkitFromArray<TInput>(array));
-  }
+	/**
+	 * Creates a new instance from an array
+	 * @param array
+	 * @alias fromIterable
+	 */
+	static fromArray<TInput>(
+		array: ReadonlyArray<TInput>,
+	): AsynkitInterface<TInput> {
+		return new Asynkit<TInput>(asynkitFromArray<TInput>(array));
+	}
 
-  /**
-   * Creates a new instance from an iterable
-   * @param it
-   */
-  static fromIterable<TInput>(it: Iterable<TInput>): AsynkitInterface<TInput> {
-    return new Asynkit<TInput>(asynkitFromIterable(it));
-  }
+	/**
+	 * Creates a new instance from an iterable
+	 * @param it
+	 */
+	static fromIterable<TInput>(it: Iterable<TInput>): AsynkitInterface<TInput> {
+		return new Asynkit<TInput>(asynkitFromIterable(it));
+	}
 
-  async *[Symbol.asyncIterator](): AsyncIterator<TInput> {
-    for await (const value of this.it) {
-      yield value;
-    }
-  }
+	async *[Symbol.asyncIterator](): AsyncIterator<TInput> {
+		for await (const value of this.it) {
+			yield value;
+		}
+	}
 
-  /**
-   * @inheritDoc
-   */
-  map<TReturn>(map: Map<TInput, TReturn>): AsynkitInterface<TReturn> {
-    return new Asynkit(asynkitMap(this.it, map));
-  }
+	/**
+	 * @inheritDoc
+	 */
+	map<TReturn>(map: Map<TInput, TReturn>): AsynkitInterface<TReturn> {
+		return new Asynkit(asynkitMap(this.it, map));
+	}
 
-  /**
-   * @inheritDoc
-   */
-  filter<TReturn extends TInput = TInput>(
-    filter: Filter<TInput, TReturn>,
-  ): AsynkitInterface<TReturn> {
-    return new Asynkit(asynkitFilter<TInput, TReturn>(this.it, filter));
-  }
+	/**
+	 * @inheritDoc
+	 */
+	filter<TReturn extends TInput = TInput>(
+		filter: Filter<TInput, TReturn>,
+	): AsynkitInterface<TReturn> {
+		return new Asynkit(asynkitFilter<TInput, TReturn>(this.it, filter));
+	}
 
-  /**
-   * @inheritDoc
-   */
-  toArray(): Promise<TInput[]> {
-    return asynkitToArray(this.it);
-  }
+	/**
+	 * @inheritDoc
+	 */
+	toArray(): Promise<TInput[]> {
+		return asynkitToArray(this.it);
+	}
 
-  /**
-   * @inheritDoc
-   */
-  firstOrDefault(def?: TInput): Promise<TInput | undefined> {
-    return asynkitFirstOrDefault(this.it, def);
-  }
+	/**
+	 * @inheritDoc
+	 */
+	firstOrDefault(def?: TInput): Promise<TInput | undefined> {
+		return asynkitFirstOrDefault(this.it, def);
+	}
 
-  /**
-   * @inheritDoc
-   */
-  first(): Promise<TInput> {
-    return asynkitFirst(this.it);
-  }
+	/**
+	 * @inheritDoc
+	 */
+	first(): Promise<TInput> {
+		return asynkitFirst(this.it);
+	}
 
-  /**
-   * @inheritDoc
-   */
-  isEmpty(): Promise<boolean> {
-    return asynkitIsEmpty(this.it);
-  }
+	/**
+	 * @inheritDoc
+	 */
+	isEmpty(): Promise<boolean> {
+		return asynkitIsEmpty(this.it);
+	}
 
-  /**
-   * @inheritDoc
-   */
-  chunk(chunkSize: number): AsynkitInterface<TInput[]> {
-    return new Asynkit(asynkitChunk(this.it, chunkSize));
-  }
+	/**
+	 * @inheritDoc
+	 */
+	chunk(chunkSize: number): AsynkitInterface<TInput[]> {
+		return new Asynkit(asynkitChunk(this.it, chunkSize));
+	}
 
-  /**
-   * @inheritDoc
-   */
-  append(...values: ReadonlyArray<TInput>): AsynkitInterface<TInput> {
-    return new Asynkit(asynkitAppend(this.it, ...values));
-  }
+	/**
+	 * @inheritDoc
+	 */
+	append(...values: ReadonlyArray<TInput>): AsynkitInterface<TInput> {
+		return new Asynkit(asynkitAppend(this.it, ...values));
+	}
 
-  /**
-   * @inheritDoc
-   */
-  prepend(...values: ReadonlyArray<TInput>): AsynkitInterface<TInput> {
-    return new Asynkit(asynkitPrepend(this.it, ...values));
-  }
+	/**
+	 * @inheritDoc
+	 */
+	prepend(...values: ReadonlyArray<TInput>): AsynkitInterface<TInput> {
+		return new Asynkit(asynkitPrepend(this.it, ...values));
+	}
 
-  /**
-   * @inheritDoc
-   */
-  some(predicate: Predicate<TInput>): Promise<boolean> {
-    return asynkitSome(this.it, predicate);
-  }
+	/**
+	 * @inheritDoc
+	 */
+	some(predicate: Predicate<TInput>): Promise<boolean> {
+		return asynkitSome(this.it, predicate);
+	}
 
-  /**
-   * @inheritDoc
-   */
-  every(predicate: Predicate<TInput>): Promise<boolean> {
-    return asynkitEvery(this.it, predicate);
-  }
+	/**
+	 * @inheritDoc
+	 */
+	every(predicate: Predicate<TInput>): Promise<boolean> {
+		return asynkitEvery(this.it, predicate);
+	}
 
-  /**
-   * @inheritDoc
-   */
-  reduce<TReturn>(
-    acc: Accumulator<TInput, TReturn>,
-    start: TReturn,
-  ): Promise<TReturn> {
-    return asynkitReduce(this.it, acc, start);
-  }
+	/**
+	 * @inheritDoc
+	 */
+	reduce<TReturn>(
+		acc: Accumulator<TInput, TReturn>,
+		start: TReturn,
+	): Promise<TReturn> {
+		return asynkitReduce(this.it, acc, start);
+	}
 
-  /**
-   * @inheritDoc
-   */
-  toObject<TKey extends keyof TInput>(
-    keySelector: KeySelector<TInput>,
-  ): Promise<Record<TKey, TInput>> {
-    return asynkitToObject(this.it, keySelector);
-  }
+	/**
+	 * @inheritDoc
+	 */
+	toObject<TKey extends keyof TInput>(
+		keySelector: KeySelector<TInput>,
+	): Promise<Record<TKey, TInput>> {
+		return asynkitToObject(this.it, keySelector);
+	}
 
-  /**
-   * @inheritDoc
-   */
-  concat(...iterables: ReadonlyArray<AsyncOrIterable<TInput>>): AsynkitInterface<TInput> {
-    return new Asynkit(asynkitConcat(this.it, ...iterables));
-  }
+	/**
+	 * @inheritDoc
+	 */
+	concat(
+		...iterables: ReadonlyArray<AsyncOrIterable<TInput>>
+	): AsynkitInterface<TInput> {
+		return new Asynkit(asynkitConcat(this.it, ...iterables));
+	}
 
-  /**
-   * @inheritDoc
-   */
-  flatten<TReturn>(this: Asynkit<AsyncOrIterable<TReturn>>): AsynkitInterface<TReturn> {
-    return new Asynkit(asynkitFlatten<TReturn>(this.it));
-  }
+	/**
+	 * @inheritDoc
+	 */
+	flatten<TReturn>(
+		this: Asynkit<AsyncOrIterable<TReturn>>,
+	): AsynkitInterface<TReturn> {
+		return new Asynkit(asynkitFlatten<TReturn>(this.it));
+	}
 
-  /**
-   * @inheritDoc
-   */
-  flatMap<TReturn>(map: Map<TInput, AsyncOrIterable<TReturn>>): AsynkitInterface<TReturn> {
-    return new Asynkit(asynkitFlatMap(this.it, map));
-  }
+	/**
+	 * @inheritDoc
+	 */
+	flatMap<TReturn>(
+		map: Map<TInput, AsyncOrIterable<TReturn>>,
+	): AsynkitInterface<TReturn> {
+		return new Asynkit(asynkitFlatMap(this.it, map));
+	}
 
-  /**
-   * @inheritDoc
-   */
-  limit(count: number): AsynkitInterface<TInput> {
-    return new Asynkit(asynkitLimit(this.it, count));
-  }
+	/**
+	 * @inheritDoc
+	 */
+	limit(count: number): AsynkitInterface<TInput> {
+		return new Asynkit(asynkitLimit(this.it, count));
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	each(fn: (value: TInput) => PromiseOrValue<void>): AsynkitInterface<TInput> {
+		return new Asynkit(asynkitEach(this.it, fn));
+	}
 }
